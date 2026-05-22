fee091c41b45547f67110e65f3a05a46835563ef
Syauqi Izzet <syauqifizan@gmail.com>
2026-05-23 02:21:43 +0700

    update fin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
     use WithoutModelEvents;
 
     /**
-     * Seed the application's database.
+     * Seed the application's databases.
      */
     public function run(): void
     {
